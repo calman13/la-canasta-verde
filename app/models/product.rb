a5950a36fd1886c3b1a40b01cd277244e0946ff1
osciel morales <osci.morales.m@gmail.com>
2021-08-09 11:31:26 -0400

    add validates product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :shop
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_float: true }
+  validates :amount, presence: true, numericality: { only_integer: true }
 end
