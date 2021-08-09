87a563593b9be474b5ab7ccee77a8df832f031c8
osciel morales <osci.morales.m@gmail.com>
2021-08-09 13:11:19 -0400

    validates product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_float: true }
-  validates :amount, presence: true, numericality: { only_integer: true }
+  validates :amount, presence: true, numericality: { only_integer: true } # preguntar
 end
