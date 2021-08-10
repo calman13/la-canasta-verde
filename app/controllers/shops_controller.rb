cbe8809d5da42e099f5bdae6c35655729b69d887
osciel morales <osci.morales.m@gmail.com>
2021-08-10 11:42:55 -0400

    Rails new with frontend and form gems

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,12 @@
 class ShopsController < ApplicationController
+  def index
+    @shops = Shop.all
+  end
+
+  def show
+    @shop = Shop.new
+    #product = Product.new
+    @product = Product.new(shop: @shop)
+    #@user = User.new(shop: @shop)
+  end
 end
