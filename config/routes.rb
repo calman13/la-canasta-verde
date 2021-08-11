a7815ed736a49ed005838c6c55d6ff5646848e5b
simon velasquez <root.matryx@gmail.com>
2021-08-11 19:24:50 -0400

    conflicto resuelto

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
   resources :users
-<<<<<<< HEAD
-  resources :shops
-=======
   resources :shops, only: [ :new, :create, :show ]
-  
->>>>>>> master
+  resources :products, only: [:show,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get 'products,to:products#index', as: :show_product
   #get 'products/: id, to:product#show',as: :products
-  resources :products, only: [:show,:index]
+  
 end
