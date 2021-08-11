2859c641491ddbf6db3ccc029855a6dcd28ef05c
sandra navarro <pilarnv@gmail.com>
2021-08-11 10:22:49 -0400

    avance show product

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'products,to:products#index', as: :show_product
+  #get 'products/: id, to:product#show',as: :products
+  resources :products, only: [:show,:index]
 end
